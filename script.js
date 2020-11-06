6674cd4abad38cec06881d59aa54d2aa4ca96d8a
Henry Hamilton Holmes <hholmes726@gmail.com>
2020-11-06 18:13:22 -0500

    Created basic structure of my array variable containing (1) each question, and (2) each answer option.

---
+++ script.js
@@ -0,0 +1,44 @@
+var questionsArray = [
+    {
+        Question: "What is number 1?",
+        answerOptions: [
+            "1.",
+            "2.",
+            "3.",
+            "4.",
+            "5.",
+        ]
+        Question: "What is number 2?",
+        answerOptions: [
+            "1.",
+            "2.",
+            "3.",
+            "4.",
+            "5.",
+        ]
+        Question: "What is number 3?",
+        answerOptions: [
+            "1.",
+            "2.",
+            "3.",
+            "4.",
+            "5.",
+        ]
+        Question: "What is number 4?",
+        answerOptions: [
+            "1.",
+            "2.",
+            "3.",
+            "4.",
+            "5.",
+        ]
+        Question: "What is number 5?",
+        answerOptions: [
+            "1.",
+            "2.",
+            "3.",
+            "4.",
+            "5.",
+        ]
+    }
+]
