a329374f6e84cf7c821d5371f3bc54c9f1d35b95
Henry Hamilton Holmes <hholmes726@gmail.com>
2020-11-06 22:11:17 -0500

    At this time, this is as far as I was able to get with putting together a timed quiz using HTML and JavaScript.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Web-APIs_Code-Quiz
+
+This is my first attempt at a timed quiz using HTML and JavaScript.
+
+I created this quiz to showcase my ability to build a timed coding quiz with multiple-choice questions, that is run in the browser, and that features dynamially updated HTML and CSS powered by JavaScript code.
+
+During this assignment, I learned how difficult it is to setup JavaScript arrays with questions and answers, have them progress as each answer is submitted until finally reaching a "High Score" page, have my code add total points for correct answers to a "High Score" section or subtract time from my quiz timer, and have my two files interact with one another, and save scores in a user's local storage on their browser. Ultimately, I was unable to build a dynamic web page beyond a countdown timer.
--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 var quizTimer = document.querySelector(".quizTimer");
 var quizBody = document.getElementById("quizBody");
 
+var quizScore = 0;
+
+for (var i = 0; i < quizQuestions.length; i++) {
+    window.prompt(quizQuestions[i].prompt);
+    if (response == quizQuestions[i].answer);
+    score++;
+    alert("You're right!");
+} else {
+    quizTimer--;
+    alert("You're wrong!");
+}
+
+var quizQuestions = [
+    {
+        prompt: "What number is 1?\n(a) 1\n\ (b) 2\n(c) 3",
+        answer: "a"
+    },
+    {
+        prompt: "What number is 2?\n(a) 1\n\ (b) 2\n(c) 3",
+        answer: "b"
+    },
+    {
+        prompt: "What number is 3?\n(a) 1\n\ (b) 2\n(c) 3",
+        answer: "c"
+    },
+]
+
 var secondsRemaining = 30;
 
 function setTime() {
-    var timerInterval = setInterval(function() {
+    var timerInterval = setInterval(function () {
         secondsRemaining--;
         quizTimer.textContent = secondsRemaining + " seconds left until your quiz is finished.";
 
-        if(secondsRemaining === 0) {
+        if (secondsRemaining === 0) {
             clearInterval(timerInterval);
             sendMessage();
         }
@@ -18,72 +45,12 @@ function setTime() {
 function sendMessage() {
     quizTimer.textConent = " ";
 
-    var placeholderImage = document.createElement("img");
+    // var placeholderImage = document.createElement("img");
 
-    placeholderImage.setAttribute("src", "https://i.pinimg.com/originals/2e/d6/cf/2ed6cf351143373b3f82dd56a1a1e84a.jpg");
-    quizBody.appendChild(placeholderImage)
-;
+    // placeholderImage.setAttribute("src", "https://i.pinimg.com/originals/2e/d6/cf/2ed6cf351143373b3f82dd56a1a1e84a.jpg");
+    // quizBody.appendChild(placeholderImage)
+    ;
 
 }
 
 setTime();
-// var quizTimer = document.querySelector(".quizTimer");
-// var quizBody = document.getElementById("#quizBody");
-// var secondsRemaining = 15;
-
-// function beginTimer() {
-//     var totalTime = setTimer(function() {
-//         secondsRemaining--;
-//         quizTimer.textContent = secondsRemaining + "seconds left in Web APIs Code Quiz.";
-
-//         if(secondsRemaining === 0) {
-//             clearInterval(totalTime);
-            
-//             // How do I redirect user to "High Scores" HTML & JavaScript files?
-//         }
-//     })
-// }
-// var questionsArray = [
-//     {
-//         Question: "What is number 1?",
-//         answerOptions: [
-//             "1.",
-//             "2.",
-//             "3.",
-//             "4.",
-//             "5.",
-//         ]
-//         Question: "What is number 2?",
-//         answerOptions: [
-//             "1.",
-//             "2.",
-//             "3.",
-//             "4.",
-//             "5.",
-//         ]
-//         Question: "What is number 3?",
-//         answerOptions: [
-//             "1.",
-//             "2.",
-//             "3.",
-//             "4.",
-//             "5.",
-//         ]
-//         Question: "What is number 4?",
-//         answerOptions: [
-//             "1.",
-//             "2.",
-//             "3.",
-//             "4.",
-//             "5.",
-//         ]
-//         Question: "What is number 5?",
-//         answerOptions: [
-//             "1.",
-//             "2.",
-//             "3.",
-//             "4.",
-//             "5.",
-//         ]
-//     }
-// ];
